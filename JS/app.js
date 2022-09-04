2cb8bd1bef194b4a55634f8895f6d78cc850f40a
Ifazo <ziaulkarimifaz@gmail.com>
2022-09-04 19:47:46 +0600

    card modal added

--- JS/app.js
+++ JS/app.js
@@ -12,7 +12,7 @@ const displayMenu = (menus) => {
     const menuDiv = document.createElement("div");
     menuDiv.classList.add("col");
     menuDiv.innerHTML = `
-      <button class="btn btn-primary" onclick="loadUrl('${menu.category_id}')">${navContainer}</button>
+      <button class="btn btn-outline-primary" onclick="loadUrl('${menu.category_id}')">${navContainer}</button>
       `;
     menuContainer.appendChild(menuDiv);
   }
@@ -30,30 +30,65 @@ const displayCard = (cards) => {
   const cardContainer = document.getElementById("card-container");
   cardContainer.textContent = "";
   for (const card of cards.data) {
-    // console.log(card.author.name);
+    // console.log(card);
     cardDiv = document.createElement("div");
     cardDiv.classList.add("col");
     cardDiv.innerHTML = `
-          <div class="row">
+          <div class="row m-2">
               <div class="col-md-4">
-                    <img src="${card.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+                    <img src="${card.image_url}" class="img-fluid rounded-start" alt="...">
               </div>
               <div class="col-md-8">
                     <div class="card-body">
-                        <h5 class="card-title">${card.title}</h5>
+                        <h6 class="card-title">${card.title}</h6>
                         <p class="card-text">${card.details}</p>
                         <div class="d-flex justify-content-between">
                         <div class="d-flex justify-content-start">
-                        <img class="img-fluid rounded-circle" style="width: 50px; height: 50px;" src="${card.author.img}" alt="avater">
+                        <img class="img-fluid rounded-circle" style="width: 30px; height: 30px;" src="${card.author.img}" alt="avater">
                         <p class="card-text"><small class="text-muted">Author : ${card.author.name}</small></p>
                         </div>
                         <div><p class="card-text"><small class="text-muted">Views: ${card.total_view}</small></p></div>
+                        <div><button type="button" class="m-2 btn btn-outline-primary" data-bs-toggle="modal"
+                        data-bs-target="#exampleModal${card._id}">Preview</button></div>
                         </div>
                     </div>
               </div>
           </div>
   `;
     cardContainer.appendChild(cardDiv);
+  }
+
+  const modalContainer = document.getElementById("modal-container");
+  modalContainer.textContent = "";
+  for (const card of cards.data) {
+  // console.log(card);
+      modalDiv = document.createElement("div");
+      modalDiv.classList.add("col");
+      modalDiv.innerHTML = `
+      <div class="modal fade" id="exampleModal${card._id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">News Preview</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <h5 class="card-title">${card.title}</h5>
+            <img src="${card.image_url}" class="img-fluid rounded-start" alt="...">
+                          <p class="card-text">${card.details}</p>
+                          <div class="d-flex justify-content-between">
+                          <div class="d-flex justify-content-start">
+                          <img class="img-fluid rounded-circle" style="width: 50px; height: 50px;" src="${card.author.img}" alt="avater">
+                          <p class="card-text"><small class="text-muted">Author : ${card.author.name}</small></p>
+                          </div>
+                          <div><p class="card-text"><small class="text-muted">Views: ${card.total_view}</small></p></div>
+                          </div>
+                      </div>
+              </div>
+        </div>
+  </div>
+      `;
+      modalContainer.appendChild(modalDiv);
   }
 };
 
