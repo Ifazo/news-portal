fb1fa6aa00c2a72dd42bc7a769a16b615554e65c
Ifazo <ziaulkarimifaz@gmail.com>
2022-09-04 07:01:49 +0600

    card dynamic success

--- JS/app.js
+++ JS/app.js
@@ -18,20 +18,29 @@ const displayMenu = (menus) => {
 };
 
 const loadUrl = (category_id) => {
-    const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
-    // console.log(url);
+  const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
+  // console.log(url);
   fetch(url)
     .then((res) => res.json())
-    .then((data) => menuDetail(data));
+    .then((data) => displayCard(data));
 };
 
-const menuDetail = (menu) => {
-    console.log(menu);
+const displayCard = (cards) => {
+  const cardContainer = document.getElementById("card-container");
+  for (const card of cards.data) {
+    // console.log(card.author.name);
+    cardDiv = document.createElement("div");
+    cardDiv.classList.add("col");
+    cardDiv.innerHTML = `
+    <p>${card.author.name}</p>
+  `;
+    cardContainer.appendChild(cardDiv);
+  }
 }
-
 
 loadMenu();
 
-{
-  /* <a href="https://openapi.programming-hero.com/api/news/category/{category_id}">${navContainer}</a> */
-}
+// const cardAuthorImg = card.data[0].image_url;
+// const cardTitle = card.data[0].title;
+// const cardImg = card.data[0].thumbnail_url;
+// const cardView = card.data[0].total_view;
